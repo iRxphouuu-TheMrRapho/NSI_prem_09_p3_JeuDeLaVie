0e85e2a173e30ffd622463c907619c4fdef14ff5
KC iRxphouuu <104188698+iRxphouuu-TheMrRapho@users.noreply.github.com>
2022-05-30 15:26:46 +0200

    Add files via upload

--- jdlv_my_tools.py
+++ jdlv_my_tools.py
@@ -15,7 +15,6 @@ from PyQt5.QtWidgets import *
 from jdlv_data import *
 from jdlv_model import *
 from jdlv_outils import *
-#from jdlv_controleur import *
 
 def clean_grid (grid):
     for i in range (default_grid_size):
@@ -56,361 +55,67 @@ def apply_game_of_life_rules (grid):
             previous_status = cases [i][j]['s']
             voisins = get_voisins (cases, i, j)
             nbre_alive_voisins = count_alive_voisins (voisins)
-            if nbre_alive_voisins == 3:
+#FAIT UN LABYRINTHE
+#            if nbre_alive_voisins == 1:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#                next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#CARRE
+#            if nbre_alive_voisins == 2:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 5:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#               next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#TRUC DROGUE
+##            if nbre_alive_voisins == 2:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins <= 3 or nbre_alive_voisins >= 7:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#                next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#CLIGNOTANT SYM
+            if nbre_alive_voisins == 1:
                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
+            elif nbre_alive_voisins <= 0 or nbre_alive_voisins >= 3:
                next_cases [i] [j] = kill_case (next_cases [i] [j])
             else:
                next_cases [i] [j] = cases [i] [j]
     return next_grid
-
-
-def R0_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            nbre_alive_voisins = count_alive_voisins (voisins)
-            if nbre_alive_voisins == 3:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R1_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 2 or nbre_alive_voisins >= 5:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R2_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 3 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R3_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 2:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R4_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 4:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 6:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R5_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 2:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R6_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R7_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 2 or nbre_alive_voisins >= 5:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R8_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 3 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R9_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 4:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 6:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R10_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 0:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 10 or nbre_alive_voisins >= 50:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-
-def R11_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 2:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins < -1 or nbre_alive_voisins >= 8:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R12_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 0 or nbre_alive_voisins >= 3:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R13_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 0:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins < 0 or nbre_alive_voisins >= 5:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R14_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 1:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 4:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R15_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 2:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 1 or nbre_alive_voisins >= 5:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-def R16_rules(grid):
-    previous_grid = grid
-    previous_cases = previous_grid.cases
-    cases = grid.cases # cases is a list of lists of dictionnaries
-    next_grid = Grid (len (cases))
-    next_cases = next_grid.cases
-    for i in range (1, len (cases) - 1):
-        for j in range (1, len (cases) - 1):
-            previous_status = cases [i][j]['s']
-            voisins = get_voisins (cases, i, j)
-            if nbre_alive_voisins == 2:
-                next_cases [i] [j] = revive_case (next_cases [i] [j])
-            elif nbre_alive_voisins <= 3 or nbre_alive_voisins >= 7:
-                next_cases [i] [j] = kill_case (next_cases [i] [j])
-            else:
-                next_cases [i] [j] = cases [i] [j]
-    return next_grid
-
-
-def manages_rules(texte):
-    if texte == "R0":
-        apply_game_of_life_rules = R0_rules
-    elif texte == "R1":
-        apply_game_of_life_rules = R1_rules
-    elif texte == "R2":
-        apply_game_of_life_rules = R2_rules
-    elif texte == "R3":
-        apply_game_of_life_rules = R3_rules
-    elif texte == "R4":
-        apply_game_of_life_rules = R4_rules
-    elif texte == "R5":
-        apply_game_of_life_rules = R5_rules
-    elif texte == "R6":
-        apply_game_of_life_rules = R6_rules
-    elif texte == "R7":
-        apply_game_of_life_rules = R7_rules
-    elif texte == "R8":
-        apply_game_of_life_rules = R8_rules
-    elif texte == "R9":
-        apply_game_of_life_rules = R9_rules
-    elif texte == "R10":
-        apply_game_of_life_rules = R10_rules
-    elif texte == "R11":
-        apply_game_of_life_rules = R11_rules
-    elif texte == "R12":
-        apply_game_of_life_rules = R12_rules
-    elif texte == "R13":
-        apply_game_of_life_rules = R13_rules
-    elif texte == "R14":
-        apply_game_of_life_rules = R14_rules
-    elif texte == "R15":
-        apply_game_of_life_rules = R15_rules
-    elif texte == "R16":
-        apply_game_of_life_rules = R16_rules
-    return apply_game_of_life_rules
-
+#            if nbre_alive_voisins == 0:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins < 0 or nbre_alive_voisins >= 5:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#               next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#            if nbre_alive_voisins == -1:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins < -1 or nbre_alive_voisins >= 8:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#               next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#GIGA CLIGNOTANT
+#            if nbre_alive_voisins == 0:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins <= 10 or nbre_alive_voisins >= 50:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#               next_cases [i] [j] = cases [i] [j]
+#    return next_grid
+#            if nbre_alive_voisins == 3:
+#                next_cases [i] [j] = revive_case (next_cases [i] [j])
+#            elif nbre_alive_voisins <= 3 or nbre_alive_voisins >= 3:
+#               next_cases [i] [j] = kill_case (next_cases [i] [j])
+#            else:
+#               next_cases [i] [j] = cases [i] [j]
+#    return next_grid
 
 def apply_rules (grid, cpt):
     # if (cpt  + 1) % 20 != 0:
@@ -432,4 +137,3 @@ def apply_rules (grid, cpt):
     #     next_grid = grid
     next_grid = apply_game_of_life_rules (grid)
     return next_grid
-
